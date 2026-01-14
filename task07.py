0f0b00ba694eeff87b7d1b1f06137092a0faf5ef
Nodirbek <berdimurodovnodir89@gmail.com>
2026-01-14 17:14:48 +0500

    bajqrildi

---
+++ task07.py
@@ -0,0 +1,21 @@
+input_file = 'input/numbers.txt'
+input_file_obj = open(input_file)
+content = input_file_obj.read()
+
+numbers = content.split()
+numbers = list(map(
+    lambda num :
+    int(num),numbers
+))
+result = list(map(
+    lambda num:
+    num ** 2,
+    numbers
+))
+
+output_file = 'Output/output07.txt'
+output_file_obj = open(output_file,'w')
+output_file_obj.write(str(result))
+
+
+
