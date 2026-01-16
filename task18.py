fbc5786377bfcc4ff9c7235422b68a3a03b9535b
Nodirbek <berdimurodovnodir89@gmail.com>
2026-01-16 14:43:44 +0500

    bajarildi

--- task18.py
+++ task18.py
@@ -1,15 +1,15 @@
 input_file = 'input/students.txt'
-output_file = 'Output/output18.txt'
+output_file = 'Output/output16.txt'
 
-name = input('name: ')
+name = input('name: ').strip()
 with open(input_file) as input_file_obj:
-   names = input_file_obj.read().split()
-result = []
-if  name in names :
-    result.append(f"{name} faylda mavjud\n")
-else :
-    result.append(f"{name} faylda mavjud emas\n")
+   names = input_file_obj.read().upper()
+if name in names:
+    print("Bu ism faylda mavjud ")
+else:
+    print("Bu ism faylda yo\'q ")
+       
 
 
 with open(output_file, 'w') as output_file_obj:
-    output_file_obj.writelines(result)
+    output_file_obj.write(names)
