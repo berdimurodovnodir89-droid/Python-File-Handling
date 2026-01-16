bf10bd671ce4a81ff6957e17050e629de61e262e
Nodirbek <berdimurodovnodir89@gmail.com>
2026-01-16 14:53:25 +0500

    bajarildi

--- task20.py
+++ task20.py
@@ -3,12 +3,16 @@ import csv
 input_file = 'Input/students.txt'
 
 with open(input_file) as csv_file:
-    reader = csv.DictReader(csv_file,fieldnames=('name','grade'))
+    reader = csv.DictReader(csv_file,fieldnames=['name','grade'])
     next(reader)
-    data = list(reader)
+    
+    total_grade = sum(map(lambda item :int(item['grade']),reader))
+    n = reader.line_num - 1
+    avg_grade = total_grade / n
 
 
 output_file = 'output/output20.txt'
-with open(output_file,'w') as csv_file:
-    writter = csv.DictWriter(csv_file,fieldnames=('name','grade'))
-    writter.writerows(data)
+with open(output_file, 'w', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=['average_grade'])
+    writer.writeheader()
+    writer.writerow({'average_grade': avg_grade})
